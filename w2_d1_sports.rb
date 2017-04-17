02a1e16697da61aad5ff1e9a9a1e61ffe81231bc
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-04-17 22:32:20 +0100

    finished compulsory for first time biatches

--- w2_d1_sports.rb
+++ w2_d1_sports.rb
@@ -1,26 +1,44 @@
 class Sports_team
 
+  attr_accessor :coach
+  attr_reader :team_name, :players, :points
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = 0
   end
 
-  def get_team_name
-    return @team_name
+  # def get_team_name
+  #   return @team_name
+  # end
+
+  # def get_players
+  #   return @players
+  # end
+
+  # def get_coach
+  #   return @coach
+  # end
+
+  # def set_new_coach(new_coach) 
+  #   @coach = new_coach
+  # end
+
+  def add_new_player(new_player)
+    @players << new_player
   end
 
-  def get_players
-    return @players
+  def check_if_player_is_in_team(player)
+    return @players.include?(player)
   end
 
-  def get_coach
-    return @coach
+  def change_points_for_result(result)
+    if result == "win" 
+      @points += 3
+    end
   end
 
-  def set_new_coach(new_coach) 
-    @coach = new_coach
-  end
 end
 
 
--- w2_d1_sports_spec.rb
+++ w2_d1_sports_spec.rb
@@ -9,21 +9,37 @@ class TestSportsTeam < MiniTest::Test
   end
 
   def test_get_team_name
-    assert_equal("Brighton", @team.get_team_name)
+    assert_equal("Brighton", @team.team_name)
   end
 
   def test_get_players
-    assert_equal(["D Stockdale", "B Saltor", "G Bong", "L Dunk", "U Humemeier", "D Stevens", "B Kayal", "J Skalak", "S Baldock", "T Hemed", "A Knockaert"], @team.get_players)
+    assert_equal(["D Stockdale", "B Saltor", "G Bong", "L Dunk", "U Humemeier", "D Stevens", "B Kayal", "J Skalak", "S Baldock", "T Hemed", "A Knockaert"], @team.players)
   end
 
   def test_get_coach
-    assert_equal("Chris Hughton", @team.get_coach)
+    assert_equal("Chris Hughton", @team.coach)
   end
 
   def test_set_new_coach
-    @team.set_new_coach("Alex Berry")
-    assert_equal("Alex Berry", @team.get_coach)
+    @team.coach = "Alex Berry"
+    assert_equal("Alex Berry", @team.coach)
   end
 
+  def test_add_new_player
+    @team.add_new_player("Eoghan")
+    assert_equal(["D Stockdale", "B Saltor", "G Bong", "L Dunk", "U Humemeier", "D Stevens", "B Kayal", "J Skalak", "S Baldock", "T Hemed", "A Knockaert", "Eoghan"], @team.players)
+  end
 
+  def test_check_if_player_is_in_team
+    assert(@team.check_if_player_is_in_team("G Bong"))
+  end
+
+  def test_get_points
+    assert_equal(0, @team.points)
+  end
+
+  def test_change_points_for_result
+    @team.change_points_for_result("win")
+    assert_equal(3, @team.points)
+  end
 end
